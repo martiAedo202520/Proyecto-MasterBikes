badf5c85566afab93b2c8019d3ef9158b25a6ea7
Tabeguto <tama.gutierrez@duocuc.cl>
2026-06-24 22:09:58 -0400

    Cambios evaluacion 24-06

--- Prueba API-Gateway/GestionDeBicicleta_Cliente/src/main/java/cl/duoc/GestionDeBicicleta_Cliente/dto/ClienteRequest.java
+++ Prueba API-Gateway/GestionDeBicicleta_Cliente/src/main/java/cl/duoc/GestionDeBicicleta_Cliente/dto/ClienteRequest.java
@@ -9,37 +9,64 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-@Schema(description = "Datos requeridos para la creación o actualización de un cliente en el sistema")
+@Schema(name = "ClienteRequest",
+        description = "DTO utilizado para registrar o actualizar clientes en el sistema MasterBikes.")
+
 public class ClienteRequest {
 
     @NotBlank(message = "El RUT es obligatorio.")
     @Pattern(regexp = "^[0-9]{7,8}-[0-9kK]{1}$", message = "El formato del RUT debe ser sin puntos y con guión (ej: 12345678-9).")
-    @Schema(description = "RUT del cliente sin puntos y con guion", example = "19876543-2")
+    @Schema(
+            description = "RUT del cliente sin puntos y con guion",
+            example = "19876543-2",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private String rut;
 
     @NotBlank(message = "El nombre es obligatorio.")
     @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres.")
-    @Schema(description = "Primer nombre del cliente", example = "Renata")
+    @Schema(
+            description = "Primer nombre del cliente",
+            example = "Renata",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private String nombre;
 
     @NotBlank(message = "El apellido es obligatorio.")
     @Size(min = 2, max = 50, message = "El apellido debe tener entre 2 y 50 caracteres.")
-    @Schema(description = "Apellidos paterno", example = "Muñoz")
+    @Schema(
+            description = "Apellido paterno del cliente",
+            example = "Muñoz",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private String apellido;
 
     @NotBlank(message = "El correo electrónico es obligatorio.")
     @Email(message = "Debe ingresar un formato de correo electrónico válido.")
-    @Schema(description = "Dirección de correo electrónico única", example = "Renata.muñoz@mail.cl")
+    @Schema(
+            description = "Dirección de correo electrónico única del cliente",
+            example = "renata.munoz@mail.cl",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private String email;
 
     @NotBlank(message = "El teléfono es obligatorio.")
-    @Schema(description = "Número telefónico móvil o fijo de contacto", example = "+56912345678")
+    @Schema(
+            description = "Número telefónico móvil o fijo de contacto del cliente",
+            example = "+56912345678",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private String telefono;
+
 
     @Schema(description = "Dirección de residencia particular (Opcional)", example = "Av. Concha y Toro 1340, Puente Alto")
     private String direccion; // Opcional, no requiere anotación obligatoria
 
     @NotNull(message = "El estado activo/inactivo es obligatorio.")
-    @Schema(description = "Define si el cliente está habilitado operativamente en la plataforma", example = "true")
+    @Schema(
+            description = "Define si el cliente se encuentra activo dentro de la plataforma",
+            example = "true",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private Boolean activo;
 }
